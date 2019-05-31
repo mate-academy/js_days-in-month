395498e18297f822e369fd952f7b98543443b649
vpolets <vpolets@gmail.com>
2019-05-31 11:24:31 +0300

    solution

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 'use strict';
 
-function isInteger(num) {
-  return (num ^ 0) === num;
-}
-
-function dateVlaidation(month, year){
-  if(isInteger(year) && isInteger(month)) {
-    if(month >= 1 && month <= 12) {
-      return true;
-    }
+function dateValidation(month, year){
+  if(
+  Number.isInteger(year) 
+  && Number.isInteger(month) 
+  && month >= 1 
+  && month <= 12
+  ) {
+    return true;
   }
   return false;
 }
 
 function daysInMonth(month, year) {
-  if(dateVlaidation(month, year)){
+  if(dateValidation(month, year)){
     const howManyDaysInThisMonth = 33 - new Date(year, month - 1, 33).getDate();
     return howManyDaysInThisMonth;
   } else {
-    throw 'Invalid input date';
+    throw new Error('Invalid input date');
   }
 }
