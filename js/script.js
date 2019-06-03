f861081b8005ac32063e374fdad2a59ac3fe42e1
Olegus <gulldan1996@gmail.com>
2019-06-03 17:24:53 +0300

    days-in-month

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use stic"
+
+function daysInMonth(month, year) {
+  try {
+    if (month > 12 || month <= 0 || !Number.isInteger(month) || !Number.isInteger(year)) {
+      throw new Error();
+    }
+    console.log(new Date(year, month, 0).getDate());
+  } catch(error) {
+    console.log('exception');
+  }
+}
+
+
+daysInMonth(5, 2019); // 31
+daysInMonth(5, '2019'); // exception
+daysInMonth(2, 2020); // 29
+daysInMonth(2.2, 2020); // exception
+daysInMonth(2, 2100); // 28
+daysInMonth(13, 2100); // exception
