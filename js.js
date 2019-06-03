5c7156d838188d637e5157db297689a9892b43f8
Antonenko Bohdan <bohdan1997@gmail.com>
2019-06-03 15:48:22 +0300

    'solution'

---
+++ js.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function daysInMonth(month, year) {
+    if (Number.isInteger(year)
+        &&Number.isInteger(month)
+        &&month <= 12) {
+        return console.log(new Date(year, month, 0 ).getDate());
+    } else {
+        return console.log('exception') ;
+    }
+}
+
+daysInMonth(5, 2019); // 31
+daysInMonth(5, '2019'); // exception
+daysInMonth(2, 2020); // 29
+daysInMonth(2.2, 2020); // exception
+daysInMonth(2, 2100); // 28
+daysInMonth(13, 2100); // exception
