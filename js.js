8c1c09a69724875c5031c71d853718fa374f9b36
Antonenko Bohdan <bohdan1997@gmail.com>
2019-06-03 16:44:10 +0300

    'changes'

--- js.js
+++ js.js
@@ -3,6 +3,7 @@
 function daysInMonth(month, year) {
     if (Number.isInteger(year)
         &&Number.isInteger(month)
+        &&month >= 0
         &&month <= 12) {
         return console.log(new Date(year, month, 0 ).getDate());
     } else {
@@ -16,3 +17,5 @@ daysInMonth(2, 2020); // 29
 daysInMonth(2.2, 2020); // exception
 daysInMonth(2, 2100); // 28
 daysInMonth(13, 2100); // exception
+daysInMonth(0, 2019);
+daysInMonth(-1, 2019);
