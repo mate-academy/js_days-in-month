a488a2c123d66e03d95432c3fccaaa6a5d962936
Ivan Deiko <ivandeiko1997@gmail.com>
2019-06-04 16:16:31 +0300

    'fix'

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 function getDaysInMonth(month, year) {
   try {
-    if (!Number.isInteger(year) && Number.isInteger(month) && month <= 12) {
-      throw new Error('incorrrect');
-    };
-
-    let daysInMonth = 33 - new Date(year, month - 1, 33).getDate();
-    return (daysInMonth);
-  } catch (err) {
-      return err.message;
+    if (Number.isInteger(year) && Number.isInteger(month) && month  <= 12) {
+      let daysInMonth = 33 - new Date(year, month - 1, 33).getDate();
+      return daysInMonth;
+    } else {
+      throw new Error("Incorrect");
+    }
+  } catch(error) {
+      return error.message;
   }
 }
-
 getDaysInMonth(5, 2019); // 31
-getDaysInMonth(5, '2019'); // exception
-getDaysInMonth(2, 2020); // 29
-getDaysInMonth(2.2, 2020); // exception
+getDaysInMonth(5, '2019') // exception
+getDaysInMonth(2, 2020.2); // 29
+getDaysInMonth(2.2, 2020) // exception
 getDaysInMonth(2, 2100); // 28
 getDaysInMonth(13, 2100); // exception
+
