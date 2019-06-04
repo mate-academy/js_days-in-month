b52164ae6342904ec55d02a9a904acec70a3b59f
annakondr <tyukondra@gmail.com>
2019-06-04 19:03:17 +0300

    solution_with_requested_changes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 'use strict';
 
 function daysInMonth(month, year) {
-
-    if (Number.isInteger(month) && Number.isInteger(year) && month < 13) {
-        let daysInMon = new Date(year, month, 0).getDate();
-        if (daysInMon === 29 || daysInMon === 28 || daysInMon === 30 || daysInMon === 31) {
-            console.log (daysInMon)
-        }
-    } else {
-        console.log('exception')
-    }
+  if (Number.isInteger(month) && Number.isInteger(year) && month < 13 && month !== 0) {
+    let daysInMon = new Date(year, month, 0).getDate();
+      console.log(daysInMon)
+  } else {
+    console.log('exception')
+  }
 }
 
 daysInMonth(5, 2019); // 31
