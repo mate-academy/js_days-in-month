eedb19bf704ebd34a9f8338353e7b821d5ea3739
Alexandr <alexandrmail32@gmail.com>
2019-06-05 11:10:15 +0300

    Elimination of double function call

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 function daysInMonth(month, year) {
   try {
-    if (!daysAmountInMonth(month, year)) {
-      throw new TypeError("exeption");
-    } 
-      return daysAmountInMonth(month, year));
-    } catch (error) {
-      return error.message;
-    }
+    return daysAmountInMonth(month, year);
+  } catch (error) {
+    return error.message;
+  }
 }
 
 function daysAmountInMonth(month, year) {
-  if ((Number.isInteger(year)&& Number.isInteger(month)) && (year > 0 && month > 0)) {
-    if (month <= 12) {
+  if ((Number.isInteger(year)&& Number.isInteger(month)) && (year > 0 && month > 0) &&month <= 12) {
       const monthFirstDay = new Date(year, month - 1, 1);
       const nextMonthFirstDay = new Date(year, month, 1);
       const daysAmount = (nextMonthFirstDay.getTime() - monthFirstDay.getTime())/(3600000*24);
       return daysAmount;
-    }
+  } else {
+    throw new TypeError("exeption");
   }
 }
 
-daysInMonth(5, 2019); // 31
-daysInMonth(5, '2019'); // exception
-daysInMonth(2, 2020); // 29
-daysInMonth(2.2, 2020); // exception
-daysInMonth(2, 2100); // 28
-daysInMonth(13, 2100); // exception
+console.log(daysInMonth(5, 2019)); // 31
+console.log(daysInMonth(5, '2019')); // exception
+console.log(daysInMonth(2, 2020)); // 29
+console.log(daysInMonth(2.2, 2020)); // exception
+console.log(daysInMonth(2, 2100)); // 28
+console.log(daysInMonth(13, 2100)); // exception
