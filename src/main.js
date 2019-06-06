0575c04f97ae3774990b93fa4b9c220d848cfeb2
annakondr <tyukondra@gmail.com>
2019-06-06 12:00:28 +0300

    new_try

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 
 function daysInMonth(month, year) {
   if (!Number.isInteger(month) || !Number.isInteger(year) || month > 12 || month <= 0) {
-    throw 'Error: exception'
+    throw new Error('Year and month must be positive integers. ' +
+      'Month must be in the range from 1 to 12 inclusive.')
   }
   return new Date(year, month, 0).getDate()
 }
