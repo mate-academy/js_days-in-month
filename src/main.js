a8216850f08db61d1a4dbefdb20ed268f25a4d4d
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-06-06 16:56:02 +0300

    With corrections

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
-function daysInMonth(month, year) {
-  try {
-    if (!Number.isInteger(year) || month <= 0 || !Number.isInteger(month) || month > 12) {
-      throw new Error("Incorrect data: false date passed");
-    }
-        
-    return new Date(year, month, 0).getDate();
+'use strict';
 
-  } catch(error) {
-    return error.message;
+function daysInMonth(month, year) {
+  if (!Number.isInteger(year) || month <= 0 || !Number.isInteger(month) || month > 12) {
+    throw new Error("Incorrect data: false date passed");
   }
+        
+  return new Date(year, month, 0).getDate();
 }
 
-daysInMonth(5, 2019); // 31
-daysInMonth(5, '2019'); // exception
-daysInMonth(2, 2020); // 29
-daysInMonth(2.2, 2020); // exception
-daysInMonth(2, 2100); // 28
-daysInMonth(13, 2100); // exception
-daysInMonth(0, 2019); // exception
+// daysInMonth(5, 2019); // 31
+// daysInMonth(5, '2019'); // exception
+// daysInMonth(2, 2020); // 29
+// daysInMonth(2.2, 2020); // exception
+// daysInMonth(2, 2100); // 28
+// daysInMonth(13, 2100); // exception
+// daysInMonth(0, 2019); // exception
+// daysInMonth(-2, 2019); // exception
