cfdb3d417a8c48cab0f89066cde388466a412580
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-22 21:29:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,9 @@ function daysInMonth(month, year) {
 }
 
 function isCorrectMonth(month) {
-  return (Number.isInteger(month) && month > 0 && month < 13);
+  return Number.isInteger(month) && month > 0 && month < 13;
 }
 
 function isCorrectYear(year) {
-  return (Number.isInteger(year));
-}
-
-function isLeapYear(year) {
-  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+  return Number.isInteger(year);
 }
