89158e668b382a53813e6b8ad716a3050c37ebca
VadimK <djvadya16@gmail.com>
2019-10-22 17:19:22 +0300

    done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function daysInMonth(month, year) {
   try {
-    if (Number.isInteger(month) && Number.isInteger(year)) {
+    if (Number.isInteger(month) && Number.isInteger(year) && month <= 12 && month > 0) {
       const dayInMonth = new Date(year, month, 0).getDate(); 
       return dayInMonth; 
     }
