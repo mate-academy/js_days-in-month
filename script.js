44a4606c8afbeb667970a484e42a0497dd9ca1bb
Anna Kazantseva <annetastara@gmail.com>
2019-10-22 17:21:11 +0300

    Fixed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function daysInMonth(month, year) {
   try {
-    if (!Number.isInteger(month) || !Number.isInteger(year) || month > 12 || year < 0) {
+    if (!Number.isInteger(month) || !Number.isInteger(year)
+      || month > 12 || month <= 0 || year <= 0) {
       throw new Error('Error');
     }
     return new Date(year, month, 0).getDate();
