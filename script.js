e4a3d3b62055ca88c3c8ba18b7a3cfa196b4a627
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-22 18:29:16 +0300

    fixed bags

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function daysInMonth(month, year) {
   try {
-    if (Number.isInteger(month) && month <= 12 && Number.isInteger(year)) return new Date(year, month, 0).getDate();
+    if (Number.isInteger(month) && (month >= 1 && month <= 12) && Number.isInteger(year)) {
+     return new Date(year, month, 0).getDate();
+    }
 
     throw new Error("parameter year or month isn't correct");
   } catch (error) {
