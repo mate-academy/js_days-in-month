3281939be7bd2615bd6d3c252183f304c13ddad2
Sijey <Popov040391@gmail.com>
2019-10-22 17:23:41 +0300

    Solution

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
 function daysInMonth(month, year) {
-    try {
-        if (Number.isInteger(month) && Number.isInteger(year) && month > 0 && month < 13) {
-            switch (month) {
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    return 31;
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    return 30;
-                case 2:
-                    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0) ? 29 : 28;
-            }
-        }
+  try {
+    if (Number.isInteger(month) && Number.isInteger(year) && month > 0 && month < 13) {
+      return new Date(year, month, 0).getDate();
+      }
 
-        throw new Error()
+    throw new Error()
 
-    } catch (err) {
-        return 'exception';
-    }
+  } catch (err) {
+    return 'exception';
+  }
 }
