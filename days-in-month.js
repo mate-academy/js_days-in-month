79e494c0f336ac3922e5a44be0c5c9bf518e34e8
nikakarelets <v.karelets@gmail.com>
2019-10-22 17:01:00 +0300

    Fix

--- days-in-month.js
+++ days-in-month.js
@@ -1,13 +1,14 @@
 function daysInMonth(month, year) {
-    try {
-        if (!Number.isInteger(month) || !Number.isInteger(year) || month > 12) {
-            throw new Error();
-        }
-        return new Date(year, month, 0).getDate();
+  try {
+    if (!Number.isInteger(month) || !Number.isInteger(year) || month > 12) {
+      throw new Error();
+      }
+      return new Date(year, month, 0).getDate();
     }
-    catch (e) {
-        return 'exception';
-    }
+
+  catch (e) {
+    return 'exception';
+  }
 }
 
 
