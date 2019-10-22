b488d751b9ae651441f2ac69aad938b3db0cb255
Veronika D <donets.veronika@yahoo.com>
2019-10-22 17:29:42 +0300

    fixed

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
 function daysInMonth(month, year){
   try {
-    if (!Number.isInteger(month && year) || month > 12) {
+    if (!Number.isInteger(month) || !Number.isInteger(year) || month > 12 || month < 1) {
       throw new Error();
     }
-
-    switch (month) {
-      case 1:
-      case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
-
-        return 31;
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-
-        return 30;
-      case 2: {
-
-        return (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) ? 29 : 28;
-      }
-    }
-    return 'exception';
+    return new Date(year, month, 0).getDate();
   }
   catch (error) {
     return 'exception';
