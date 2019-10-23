5ef9c4d3a7c33a98d93494d09f8c6429ab91bbbd
Viktor Lukovets <nuerio63@gmail.com>
2019-10-23 11:44:08 +0300

    jf

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 'use strict'
 
 function daysInMonth(month, year) {
-	try {
-		if (!Number.isInteger(year) || !Number.isInteger(month) || month > 12) {
-			throw new Error();
-		}
-		return new Date(year, month, 0).getDate();
-	} catch {
-		return 'exception';
-	}
+  try {
+    if (!Number.isInteger(year) || !Number.isInteger(month) || month > 12) {
+      throw new Error();
+    }
+    return new Date(year, month, 0).getDate();
+  } catch {
+    return 'exception';
+  }
 }
