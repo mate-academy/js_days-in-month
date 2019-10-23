4e34ccc70a02274b5ab93d6521d856b58f857bfb
Viktor Lukovets <nuerio63@gmail.com>
2019-10-23 11:47:18 +0300

    Solution

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 function daysInMonth(month, year) {
   try {
-    if (!Number.isInteger(year) || !Number.isInteger(month) || month > 12) {
+    if (!Number.isInteger(year) || !Number.isInteger(month) || month > 12 || month < 1) {
       throw new Error();
     }
     return new Date(year, month, 0).getDate();
