65336e405751f4f33c3980a4beead58b4f56b88e
YuraBobeshko <gorobech55@gmail.com>
2019-10-23 20:16:04 +0300

    sol

---
+++ src/js_days-in-month.js
@@ -0,0 +1,16 @@
+'use strike'
+
+function daysInMonth(month, year) {
+
+  try {
+    if (!Number.isInteger(year) || !Number.isInteger(month) || month > 12) {
+      throw new Error('exception');
+    }
+
+    return new Date(year, month, 0).getDate();
+  } catch (error) {
+    return error;
+  }
+}
+
+daysInMonth(15, '2019');
