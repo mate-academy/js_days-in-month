3a18064037aa5c48437303d0ee0a0f19b775a518
Simon <semen.zelenskiy@gmail.com>
2019-10-23 15:38:47 +0300

    solution

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict'
+function daysInMonth(month, year) {
+    try {
+        if (!Number.isInteger(month) || !Number.isInteger(year)
+            || month > 12 || month <= 0 || year <= 0) {
+            throw new Error('Error');
+        }
+        return new Date(year, month, 0).getDate();
+    } catch (error) {
+        return 'exception';
+    }
+}
+
+
+
+
+
+daysInMonth(5, 2019); // 31
+daysInMonth(5, '2019'); // exception
+daysInMonth(2, 2020); // 29
+daysInMonth(2.2, 2020); // exception
+daysInMonth(2, 2100); // 28
+daysInMonth(13, 2100); // exception
