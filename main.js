29bd8f42684d586aa321a9c16d83f6b22bdd1a4d
Rudenko Artem <artem.rud@bk.ru>
2019-10-23 14:28:17 +0300

    fix new Date()

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
 function daysInMonth(month, year) {
   try {
-    if (Number.isInteger(year) && Number.isInteger(month) && month <= 12) {
-      let days;
-      switch (month) {
-        case 2:
-          let leapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-          days = leapYear ? 29 : 28;
-          break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-          days = 30;
-          break;
-        default:
-          days = 31;
+    if (Number.isInteger(year) && 
+        Number.isInteger(month) && 
+        month <= 12 && month > 0) {
 
-      }
-      return days;
-
+      return new Date(year, month, 0).getDate();
     }
     throw error;
   } catch (err) {
+    
     return 'exception';
   }
 }
 
-console.log(daysInMonth(2, 2020));
+console.log(daysInMonth(2, 2019));
