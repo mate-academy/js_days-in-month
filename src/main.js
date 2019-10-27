f46ce356968dd2c4e48e5fcfd64453317825a85d
lubremets <lubremets@gmail.com>
2019-10-27 17:42:54 +0200

    use new Date

--- src/main.js
+++ src/main.js
@@ -1,26 +1,7 @@
 const daysInMonth = function(Month, Year) {
   try {
     if ((Month === parseInt(Month)) && (Year === parseInt(Year)) && (Month >= 1 && Month <= 12)) {
-      if ((Month == 2) && ((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) {
-        return 29;
-      }
-      switch (Month) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:          
-        case 12:
-          return 31;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-          return 30;
-        case 2:
-          return 28;
-      }
+      return (new Date(Year, Month, 0) - new Date(Year, Month - 1, 0)) / 86400000;
     }
     throw "exception";
   } catch(error) {
